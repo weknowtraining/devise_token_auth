8cf0e85c7ad9c4d2978782850c539db97e789f44
Joel Korpela <joel.korpela@weknowtraining.ca>
2020-07-07 11:05:29 -0600

    remove token serialization

--- app/models/devise_token_auth/concerns/active_record_support.rb
+++ app/models/devise_token_auth/concerns/active_record_support.rb
@@ -3,10 +3,6 @@ require_relative 'tokens_serialization'
 module DeviseTokenAuth::Concerns::ActiveRecordSupport
   extend ActiveSupport::Concern
 
-  included do
-    serialize :tokens, DeviseTokenAuth::Concerns::TokensSerialization
-  end
-
   class_methods do
     # It's abstract replacement .find_by
     def dta_find_by(attrs = {})
